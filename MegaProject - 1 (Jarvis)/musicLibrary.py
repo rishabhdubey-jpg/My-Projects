ef15cb67d1d978761271a9e25c126edf6938c6bd
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-07-09 12:49:27 +0530

    Add files via upload

---
+++ MegaProject - 1 (Jarvis)/musicLibrary.py
@@ -0,0 +1,12 @@
+music = {
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "tereliye" : "https://youtu.be/AlvUuGJccKs?si=p6Xt5VhnGaYv2YkW",
+    "runaway" : "https://youtu.be/d_HlPboLRL8?si=-KP3oG8hTgztKV5B",
+    "no lie" : "https://youtu.be/GzU8KqOY8YA?si=K4eO_2J95cgApfRJ",
+    "diet mountain dew" : "https://youtu.be/IGT0kzjUzIs?si=-b_NG5uS9j5-ztAD",
+    "i think they call this love" : "https://youtu.be/CnEqrgMlWLQ?si=-e4u7ODSPaG5f5-J",
+    "naamtera" : "https://youtu.be/fEaF31ojyjQ?si=-hwW8IEfvUzC-tJI",
+    "hundred dollar bill" : "https://youtu.be/Db6agYL2w6A?si=FXYfeRPkqcNdsFTv",
+    "bhutto" : "https://youtu.be/HZX-z8MsuDY?si=Ttvaowlvy-c4DxWq",
+    "high on you" : "https://youtu.be/-IrHSWo-MIY?si=fsv1vGzree_aQRjc",
+}
